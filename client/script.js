c58127ef9a57197291ddaf0c0d54208beebd7701
Prashant Priyadarshi <93637265+PriyadarshiPrashant@users.noreply.github.com>
2023-03-21 22:10:37 +0530

    linkchanged

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async(e) => {
 
     //Fetch data from server -> bot's response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chatbotai-pj0m.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
